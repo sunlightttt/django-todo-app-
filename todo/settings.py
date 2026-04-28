122e68e359bdb4aecf3a02afdd08216a36665410
sunlightttt <spsunlighte@gmail.com>
2026-04-28 16:45:02 +0530

    added vercel deployment config

--- todo/settings.py
+++ todo/settings.py
@@ -115,4 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
